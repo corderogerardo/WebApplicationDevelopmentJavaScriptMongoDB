60ad7e036f246c9a8f5a3e696d8c614f6fd9caa6
Gerardo Cordero <cordero.gerard@gmail.com>
2015-12-21 00:15:26 -0430

    week 1 meteor set up - bootstrap - app textcircle

---
+++ textcircle/textcircle.js
@@ -0,0 +1,31 @@
+this.Documents = new Mongo.Collection('documents');
+
+if (Meteor.isClient) {
+ 	Template.editor.helpers({
+ 		docid: function () {
+ 			var doc = Documents.findOne();
+ 			if(doc){
+	 			return doc._id;
+ 			}else{
+ 				return undefined;
+ 			}
+ 		},
+ 		config:function(){
+ 			return (function(editor){
+ 				editor.on("change", function(cm_editor,info){
+ 				console.log(cm_editor.getValue());
+ 				$("#viewer_iframe").contents().find("html").html(cm_editor.getValue())
+ 				});
+ 			});
+ 		},
+ 	});
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+    if (!Documents.findOne()) { //no documents yet
+    	Documents.insert({title:"My new document"});
+    }
+  });
+}
